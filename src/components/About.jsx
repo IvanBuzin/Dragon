85eaabc7e71b7cecd07b8f4ece42a090c6bb0129
IvanBuzin <ua.buzin@gmail.com>
2024-08-12 22:14:50 +0300

    video

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,7 +30,7 @@ const About = () => {
     setTimeout(() => {
       const data = {
         // description: "",
-        video_url: "https://www.youtube.com/embed/L-CfAaqL-4g",
+        video_url: "https://www.youtube.com/watch?v=921VbEMAwwY&t=1s",
       };
       setAboutData(data);
       setLoading(false);
